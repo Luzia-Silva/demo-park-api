c9568273a5c8f7389cf7b9202bba12655c5423cf
Luzia-Silva <aluziagabriela@gmail.com>
2024-01-14 21:50:47 -0300

    Implementação de testes

---
+++ src/test/java/com/girludev/demoparkapi/UserIT.java
@@ -0,0 +1,53 @@
+package com.girludev.demoparkapi;
+
+import com.girludev.demoparkapi.web.dto.UserCreateDTO;
+import com.girludev.demoparkapi.web.dto.UserResponseDTO;
+import com.girludev.demoparkapi.web.exception.ErrorMessage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)//ANTES que metodo for executado é necessário que estaja no banco os registros
+@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)// APOS o metodo ser executado que o delete irá ocorrer
+
+public class UserIT {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void createUser_usernameAndPasswordValid_returnUserCreatedWithStatus201(){
+     UserResponseDTO  responseBody =  webTestClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDTO("anajulia@gmail.com", "123456789"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UserResponseDTO.class)
+                .returnResult().getResponseBody();
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("anajulia@gmail.com");
+    org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
+    @Test
+    public void createUser_usernameInvalid_returnErrorMessageStatus422() {
+        ErrorMessage responseBody = webTestClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDTO("", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+}
