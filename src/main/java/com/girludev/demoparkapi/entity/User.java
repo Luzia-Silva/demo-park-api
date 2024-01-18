018579e75b9f298b2dd688ea4c74c6326b3625db
Luzia-Silva <aluziagabriela@gmail.com>
2024-01-17 21:46:37 -0300

    Inserção de auditoria

--- src/main/java/com/girludev/demoparkapi/entity/User.java
+++ src/main/java/com/girludev/demoparkapi/entity/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -12,25 +17,36 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User  {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+
 	@Column(name = "username", nullable = false, unique = true, length = 100)
 	private String username;
+
 	@Column(name = "password", nullable = false, length = 200)
 	private String password;
+
 	@Enumerated(EnumType.STRING)
 	@Column(name = "role", nullable = false, length = 25)
 	private Role  role = Role.ROLE_CLIENTE;
-	
+
+	@CreatedDate
 	@Column(name = "creation_date")
 	private LocalDateTime creation_date;
+
+	@LastModifiedDate
 	@Column(name = "modification_date")
 	private LocalDateTime dmodification_date;
+
+	@CreatedBy
 	@Column(name = "created_by")
 	private String created_by;
+
+	@LastModifiedBy
 	@Column(name = "modified_by")
 	private String modified_by;
 	
